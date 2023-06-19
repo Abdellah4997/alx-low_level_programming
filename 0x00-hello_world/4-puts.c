cf3309c98a433dd73bc2abd505c6d525b92e1547
Abdellah4997 <janjamabdellah9@gmail.com>
2023-06-19 15:55:43 -0700

    0-preprocessor

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
 *main - main entry point
+*Description: Prints the message "Programming is like building a multilingual puzzle"
+*              followed by a new line.
 *return : always 0 (success)
 */
 int main(void)
