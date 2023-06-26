9d687fc935991484520efd56cd4f5d311eb07eff
Abdellah4997 <janjamabdellah9@gmail.com>
2023-06-26 15:41:11 -0700

    0-reset_to_98.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
 *main - main entry point
-*Description: Prints the message "Programming is like building a multilingual puzzle"
-*              followed by a new line.
+*Description: Prints the message
 *return : always 0 (success)
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("*""Programming is like building a multilingual puzzle\n");
 	return (0);
 }
